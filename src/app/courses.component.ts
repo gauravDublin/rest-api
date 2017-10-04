07dd86fd90de73ddc08bf813efa8b9908170b5ce
Gaurav Kapoor <gaurav.kapoor2@mail.dcu.ie>
2017-10-04 22:58:24 +0200

    Bindings Included

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,13 +7,26 @@ import { CoursesService } from './courses.service';
               // <div id ="aId"                   "#courses"
   template: `
               <h2>{{ title }}</h2>
-              <ul>
+<!--              <ul>
                 <li *ngFor="let subject of courses">{{subject}}</li>
               </ul>
+              &lt;!&ndash;<img src="{{ imageURL }}"/>&ndash;&gt;
+              <img [src]="imageURL" height="100px" width="200px"/>
+              <table>
+                <tr>
+                  <td [attr.colspan]="columnSpan">
+                  </td>
+                </tr>
+              </table>-->
+              <button class="btn btn-primary" [class.active]="isActive">Class Binding</button>
+              <button class="btn btn-primary" [style.backgroundColor]="isActive? 'lightgreen': 'pink'">Style Binding</button>
             `
 })
 export class CoursesComponent {
     title = "Courses";
+    imageURL = "https://cdn-images-1.medium.com/max/622/1*grk7btEn0OJEQRKgG2Qs2A.png";
+    columnSpan = 2 ;
+    isActive = true;
     courses;
     constructor(service: CoursesService) {
       this.courses = service.getCoursesList();
