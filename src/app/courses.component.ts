8ca3f0bbabb769a03ea1162cd759167239851b17
kapoordcu <gaurav.kapoor@booxware.de>
2017-10-05 14:52:11 +0200

    Event Filtering added

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CoursesService } from './courses.service';
 
 @Component({
-  selector: 'courses', //<courses>                "courses"
+  selector: 'courses', // <courses>                "courses"
               // <div class = "courses"          ".courses"
               // <div id ="aId"                   "#courses"
   template: `
@@ -20,25 +20,44 @@ import { CoursesService } from './courses.service';
               </table>-->
               <button class="btn btn-primary" [class.active]="isActive">Class Binding</button>
               <button class="btn btn-primary" [style.backgroundColor]="isActive? 'lightgreen': 'pink'">Style Binding</button>
-               <div (click)="onDivClicked()">
-                  <button (click)="onSave($event)">Even Binding</button>
+              <div>
+                <br>
               </div>
+               <div (click)="onDivClicked()">
+                  <button class="btn btn-primary" (click)="onSave($event)">Even Binding</button>
+              </div>
+              <div>
+                <br>
+              </div>
+              <!--<input (keyup)="onKeyUp($event)"/> Submit Data-->
+              <input #email (keyup.enter)="onKeyUp(email.value)"/> Submit Data
             `
 })
 export class CoursesComponent {
-    title = "Courses";
-    imageURL = "https://cdn-images-1.medium.com/max/622/1*grk7btEn0OJEQRKgG2Qs2A.png";
+    title = 'Courses';
+    imageURL = 'https://cdn-images-1.medium.com/max/622/1*grk7btEn0OJEQRKgG2Qs2A.png';
     columnSpan = 2 ;
     isActive = true;
     courses;
     onSave($event) {
       $event.stopPropagation(); // STop the bubbleing
-      console.log("Btn was clicked.", $event);
+      console.log('Btn was clicked.', $event);
     }
 
-  onDivClicked() {
-    console.log("Div was clicked.");
-  }
+    onDivClicked() {
+      console.log('Div was clicked.');
+    }
+
+/*    onKeyUp($event) {
+      if ($event.keyCode === 13) {
+        console.log('Enter was pressed.');
+      }
+    }*/
+
+    onKeyUp(email) {
+      console.log('Enter was pressed.' + email);
+    }
+
     constructor(service: CoursesService) {
       this.courses = service.getCoursesList();
     }
